d039c4e66986d4fa14950a90bb28e5f75aa23e37
Milla321 <talianljudmila@gmail.com>
2020-11-07 00:36:58 +0100

    Park-project

---
+++ README.md.txt
@@ -0,0 +1,4 @@
+Park project
+Egy park bekerített részeit kell gyeptéglával befedni. 1 gyeptégla 1 m2. A két téglalap
+egyforma méretű, a rövidebb oldala 25 m, a hosszabb 75 m. A középső négyzet
+pontosan fele a téglalapoknak.
---
+++ park.py
@@ -0,0 +1,15 @@
+# Talián Milla
+# 2020.10.20
+# Esti szoft.
+
+import math
+
+print("Talián Milla, 2020.10.20, Esti szoftver")
+
+A= 75 
+B= 25 
+C=(A*B*2.5)
+
+print("Gyeptéglák száma" , (math.ceil (C)) , "db") # 0.5 db marad feleslegbe
+
+
